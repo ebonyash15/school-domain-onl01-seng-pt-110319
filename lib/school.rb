38f158b0a2e325d6100ab8ec2e18c8ae5cbdfb42
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-20 23:51:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,11 @@ class School
     @@roster
   end
   def add_student(name, grade)
+    if @@roster.include?(grade)
+      @@roster[grade] << name
+    else
       @@roster[grade]=[]
       @@roster[grade] << name
-
+    end
   end
 end
