5519c973e10ff072d275021905e743683b9152e4
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-20 23:03:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  ROSTER={}
+  def initialize(name)
+    @name=name
+  end
+  def roster=(roster)
+    ROSTER << roster
+  end
+  def roster
+    ROSTER
+  end
+  def add_student(name, grade)
+    if !ROSTER.keys.include?(grade)
+      ROSTER[grade]=[name]
+    else
+      ROSTER[grade]<<name
+    end
+  end
+end
